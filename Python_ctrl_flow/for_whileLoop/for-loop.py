6a7e89369f1852cf26a6892193989cd15ad0dbc9
Agana-Joseph <josephagana8@gmail.com>
2022-12-13 18:39:10 +0100

    add the user input in a collective sum

---
+++ Python_ctrl_flow/for_whileLoop/for-loop.py
@@ -0,0 +1,7 @@
+sum = 0
+
+for i in range(1, 101, 2):
+    print(i)
+    sum += i
+print(
+    f"This program print all the even number from 0 to 100 and the sum is ({sum})")
---
+++ Python_ctrl_flow/for_whileLoop/while-loop.py
@@ -0,0 +1,13 @@
+# Program that continously take a number from user and add it to a collective sum
+
+sum = 0
+userChoice = input("Would you like to choose a number? ")
+
+while (userChoice == 'y'):
+    userNum = input("Enter your choice of number to be added:: ")
+    sum += int(userNum)
+
+    userChoice = input("Would you like to choose a number? ")
+    print(f'The sum of the number entered is {sum}')
+else:
+    print('EXIT!')
