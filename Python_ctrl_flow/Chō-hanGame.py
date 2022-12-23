3b3af2434cc8e778be2777eede0f741b8a7fb727
Agana-Joseph <josephagana8@gmail.com>
2022-12-23 20:44:01 +0100

    [200~# Dice roll with the sum of the both dice

---
+++ Python_ctrl_flow/Chō-hanGame.py
@@ -0,0 +1,17 @@
+# Dice roll with the sum of the both dice
+
+import random
+
+diceSum = 0
+
+for i in range(2):  # the both dice
+
+    diceRoll = random.randint(0, 6)  # the total numbers of dice count
+    print('Dice rolled', diceRoll)
+    diceSum += diceRoll
+print('Dice sum is: ', diceSum)
+
+if (diceSum % 2 == 0):
+    print("Dice sum is even")
+else:
+    print("Dice sum is odd")
