6aa07ad208d0a505e529f25e22415e0c3d8efd81
Agana-Joseph <josephagana8@gmail.com>
2022-12-23 20:39:38 +0100

    Dice roll

--- Python_ctrl_flow/dice_Roll.py
+++ Python_ctrl_flow/dice_Roll.py
@@ -1,10 +1,12 @@
+# Dice roll with the sum of the both dice
+
 import random
 
 diceSum = 0
 
-for i in range(2):
+for i in range(2):  # the both dice
 
-    diceRoll = random.randint(0, 6)
+    diceRoll = random.randint(0, 6)  # the total numbers of dice count
     print('Dice rolled', diceRoll)
     diceSum += diceRoll
 print('Dice sum is: ', diceSum)
