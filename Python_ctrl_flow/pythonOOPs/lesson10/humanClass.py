f2d07592251aa5791f9b46d799b7a329395b2462
Agana-Joseph <josephagana8@gmail.com>
2023-10-11 23:13:26 +0100

    Python OOPs

---
+++ Python_ctrl_flow/pythonOOPs/lesson10/humanClass.py
@@ -0,0 +1,17 @@
+# Objects Creating my own dataType
+
+# Human class
+class Human:
+    def __init__(self, name, hairColor):
+        self.name = name
+        self.hairColor = hairColor
+
+    def introduce(self):
+        print(f"My name is {self.name} and may hair color is {self.hairColor}")
+
+
+josephOnahi = Human("Onahi", "dark")
+josephOnahi.introduce()
+
+anynyu = Human("Nerd", "blond")
+anynyu.introduce()
