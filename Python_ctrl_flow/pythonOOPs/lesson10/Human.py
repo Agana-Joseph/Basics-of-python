ef0f9a707333a84dd78fc935dbc71e7d999a7144
Agana-Joseph <josephagana8@gmail.com>
2023-10-12 23:23:58 +0100

    python OOPs

---
+++ Python_ctrl_flow/pythonOOPs/lesson10/Human.py
@@ -0,0 +1,10 @@
+class Human:
+    def __init__(self, Name):
+        self.Name = Name
+
+    def sayName(self):
+        print(self.Name)
+
+
+r = Human("Joseph")
+r.sayName()
--- Python_ctrl_flow/pythonOOPs/lesson10/card.py
+++ Python_ctrl_flow/pythonOOPs/lesson10/card.py
@@ -5,15 +5,38 @@ class Card:
         self.rank = rank
         self.point = point
 
-    def prinCard(self):
-        print("*---*")
+    def printCard(self):
+        print("*----*")
         print(f"|{self.suit}  |")
-        print(f"| {self.rank} |")
+        print(f"| {self.rank}  |")
         print(f"|  {self.suit}|")
-        print("*---*")
-        print(f"{self.point}")
+        print("*----*")
 
 
-card1 = Card("♥", "K", 10)
-card1.prinCard()
-print(card1.point)
+dec = []
+
+suits = ["💛", "💎", "💎", "🧡"]
+rankAndPoint = {
+    "A": 11,
+    "K": 11,
+    "Q": 10,
+    "J": 10,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2
+
+}
+
+for suit in suits:
+    for rank, point in rankAndPoint.items():
+        newCard = Card(suit, rank, point)
+        dec.append(newCard)
+
+for card in dec:
+    card.printCard()
